d5abaed3aec27237f6da04a4ecfcf19303dff5c5
Xingwu Li <xingwu.li@outlook.com>
2023-12-10 22:32:28 +0800

    前后端交互完成

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, jsonify, redirect, url_for, session, render_template
+from flask_uploads import UploadSet, configure_uploads, DOCUMENTS
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from werkzeug.security import generate_password_hash, check_password_hash
-from models import User, db, Case, Client
+from models import User, db, Case, Client, Document
 from flask_cors import CORS
 from middlewares import check_empty_json
 import os
@@ -16,6 +17,9 @@ app.secret_key = 'your_secret_key'  # 设置一个安全的密钥
 
 # Database Configuration
 #app.config.from_object('config_test.TestConfig')
+app.config['UPLOADED_FILES_DEST'] = '/database/file'
+files = UploadSet('files', DOCUMENTS)
+configure_uploads(app, files)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test_database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
@@ -91,7 +95,6 @@ def newClient():
     existing_client = Client.query.filter_by(citizen_id=data['citizen_id']).first()
     #调试用
     print("Received data:", data)
-    print("Existing client:", existing_client)
     if existing_client:
         return jsonify({'message': '客户已存在'}), 409
     client_id = str(uuid.uuid4())
@@ -101,27 +104,78 @@ def newClient():
     db.session.add(new_client)
     db.session.commit()
     return jsonify({'message': '客户档案添加成功！'}), 201
+
 @app.route('/newCase', methods=['POST'])
 def newCase():
     data = request.get_json()
-    case_id = str(uuid.uuid4())
-    new_case = Case(id=case_id, name=data['name'], phone=data['phone'], email=data['email'],
-                        citizen_id=data['citizen_id'], postal_code=data['postal_code'], address=data['address'])
+    existing_case = Case.query.filter_by(name=data['case_number']).first()
+    # 调试用
+    print("Received data:", data)
+    if existing_case:
+        return jsonify({'message': '案件已存在'}), 409
+    case_id = str(uuid.uuid4()) #id类型已删，下次更新数据库删除这一行
+    new_case = Case(id=case_id, client_name=data['client_name'], client_id=data['client_citizen_id'],
+                    lawyer_name=data['user_name'], lawyer_id=data['user_id'], case_number=data['case_number'],
+                    opposite_party_name=data['opposite_party_name'], case_type=data['case_type'], court=data['court'],
+                    trial_level=data['trial_level'], dispute_subject=data['dispute_subject'],
+                    agency_fee=data['agency_fee'], c_permission=data['c_permission'])
     print("New case info:", new_case)
     db.session.add(new_case)
     db.session.commit()
     return jsonify({'message': '案件添加成功！'}), 201
 
-@app.route('/search_clients', methods=['GET'])
-def search_clients():
+@app.route('/searchClients', methods=['GET'])
+def searchClients():
     query = request.args.get('query')
     # 使用SQLAlchemy查询数据库以模糊搜索用户
     clients = Client.query.filter(Client.name.ilike(f'%{query}%')).all()
     # 将结果转换为JSON并发送回前端
-    #result = [{'citizen_id': client.citizen_id, 'name': client.name} for client in clients]
-    result = [{'name': client.name} for client in clients]
+    result = [{'citizen_id': client.citizen_id, 'name': client.name} for client in clients]
     return jsonify(result)
 
+# 路由处理文件上传
+@app.route('/upload_file', methods=['POST'])
+def upload_file():
+    if 'file' in request.files:
+        file = request.files['file']
+        description = request.form.get('description', '')
+        filename = files.save(file)
+        file_url = files.url(filename)
+
+        # 创建一个新的文档对象并保存到数据库
+        new_document = Document(
+            name=filename,
+            url=file_url,
+            description=description
+        )
+        db.session.add(new_document)
+        db.session.commit()
+
+        return jsonify({'message': '文件上传成功', 'url': file_url})
+    return jsonify({'message': '没有文件上传'}), 400
+
+@app.route('/get_file_list')
+def get_file_list():
+    # 这里的逻辑取决于您是如何存储文件信息的
+    # 假设有一个函数get_all_files()返回所有文件信息
+    files = get_all_files() # 例如: [{'id': 1, 'name': 'file1.pdf', ...}, ...]
+    return jsonify(files)
+def get_all_files():
+    # 查询所有文件信息
+    documents = Document.query.all()
+    return [{'id': doc.id, 'name': doc.name, 'url': doc.url, 'description': doc.description} for doc in documents]
+
+@app.route('/delete_file/<int:file_id>', methods=['DELETE'])
+def delete_file(file_id):
+    document = Document.query.get(file_id)
+    if document:
+        db.session.delete(document)
+        db.session.commit()
+        file_path = os.path.join(app.config['UPLOADED_FILES_DEST'], document.name)
+        if os.path.exists(file_path):
+            os.remove(file_path)
+        return jsonify({'message': '文件删除成功'})
+    return jsonify({'message': '文件不存在'}), 404
 
 
 '''
@@ -195,4 +249,3 @@ def generate_salt():
 
 if __name__ == '__main__':
     app.run(port=8080, debug=True)
-
--- web/src/main.js
+++ web/src/main.js
@@ -4,13 +4,37 @@ const BASE_URL = 'http://127.0.0.1:8080';
 
 function modalControl(newModalName, closeCurrent) {
     // 获取当前打开的模态弹框
+    try {
+        var currentModal = document.querySelector(".modal.show");
+        if (closeCurrent && currentModal) {
+            var modal = new bootstrap.Modal(currentModal);
+            modal.hide();
+        }
+        
+        if (newModalName) {
+            var newClientModal = document.getElementById(newModalName);
+            if (newClientModal) {
+                var modal = new bootstrap.Modal(newClientModal);
+                modal.show();
+            } else {
+                console.error("Modal with ID '" + newModalName + "' not found.");
+            }
+        }
+    } catch (error) {
+        console.error("An error occurred:", error);
+    }
+}
+```
+function modalControl(newModalName, closeCurrent) {
+    // 获取当前打开的模态弹框
     var currentModal = document.querySelector(".modal.show");
     if (closeCurrent) {
         // 如果需要关闭当前模态弹框
         if (currentModal) {
             var modal = new bootstrap.Modal(currentModal);
-            // 手动关闭当前模态弹框（不触发 "hidden.bs.modal" 事件）
-            modal.hide();
+            setTimeout(function () {
+                modal.hide();
+            }, 100); // 100毫秒延迟，您可以根据需要进行调整
         }
     }
     if (newModalName) {
@@ -24,7 +48,7 @@ function modalControl(newModalName, closeCurrent) {
         }, 50);
     }
 }
-
+```
 //客户档案收集
 function newClient() {
     var name = document.getElementById('clientName').value;
@@ -61,41 +85,51 @@ function newClient() {
 
 //案件收集
 function newCase() {
-    var name = document.getElementById('client_name').value;
-    var opposite_party_name = document.getElementById('opposite_party_name').value;
-    var case_type = document.getElementById ('case_type').value;
-    var court = document.getElementById('court').value;
-    var dispute_subject = document.getElementById ('dispute_subject').value;
-    var agency_fee = document.getElementById ('agency_fee').value;
-    var c_permission = document.getElementById ('c_permission').value;
+    var trialLevelSelect = document.getElementById('trial_level');
+    var cPermissionSelect = document.getElementById('c_permission');
 
-    // AJAX request to Flask backend for registration
-    fetch(BASE_URL + '/newCase', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ name: name, opposite_party_name: opposite_party_name, case_type: case_type,
-            court: court, dispute_subject: dispute_subject, agency_fee: agency_fee, c_permission: c_permission }),
-    })
-    .then(response => {
-        if (response.status === 201) {
-            response.json().then(data => {
-                alert('消息：' + data.message); // 显示自定义错误消息
-                modalControl(null, true)
-            });
-         } else if (response.status === 409){
-            alert('消息：' + data.message); // 显示自定义错误消息
-         }
-         else if (response.status === 400){
-            response.json().then(data => {
-            alert('消息：' + data.error()); //输入有空项
-            });
-        }
-    });
+    if (trialLevelSelect.value === "选择审级" || cPermissionSelect.value === "选择授权选项") {
+        alert('请选择有效的审级和授权选项！');
+    } else {
+         //var client_citizen_id = document.getElementById('client_citizen_id').value;
+    var opposite_party_name = document.getElementById ('opposite_party_name').value,
+        case_number = document.getElementById ('case_number').value,
+        case_type = document.getElementById ('case_type').value, court = document.getElementById ('court').value,
+        dispute_subject = document.getElementById ('dispute_subject').value,
+        agency_fee = document.getElementById ('agency_fee').value,
+        trial_level = document.getElementById ('trial_level').value, c_permission = document.getElementById ('c_permission').value,
+        client_name = client.name, client_citizen_id = client.citizen_id, user_name = user.name, user_id = user.id;
+        // AJAX request to Flask backend for registration
+         fetch (BASE_URL + '/newCase', {
+             method: 'POST',
+             headers: {
+                 'Content-Type': 'application/json',
+             },
+             body: JSON.stringify ({
+                 client_name: client_name, client_citizen_id: client_citizen_id,
+                 opposite_party_name: opposite_party_name, user_name: user_name, user_id: user_id,
+                 case_number: case_number, case_type: case_type, court: court,
+                 dispute_subject: dispute_subject, agency_fee: agency_fee,
+                 trial_level: trial_level, c_permission: c_permission}),
+         })
+             .then (response => {
+                 if (response.status === 201) {
+                     response.json ().then (data => {
+                         alert ('消息：' + data.message); // 显示自定义错误消息
+                         modalControl (null, true)
+                     });
+                 } else if (response.status === 409) {
+                     alert ('消息：' + data.message); // 显示自定义错误消息
+                 } else if (response.status === 400) {
+                     response.json ().then (data => {
+                         alert ('消息：' + data.error ()); //输入有空项
+                     });
+                 }
+             });
+     }
 }
 
-
+//实时客户名搜索实现
 document.addEventListener('DOMContentLoaded', function() {
     var searchInput = document.getElementById('clientNameSearchInput');
     var searchResults = document.getElementById('clientNameSearchResults');
@@ -104,7 +138,7 @@ document.addEventListener('DOMContentLoaded', function() {
         var query = searchInput.value;
             // 创建XMLHttpRequest对象
             var xhr = new XMLHttpRequest();
-            xhr.open('GET', '/search_clients?query=' + query, true);
+            xhr.open('GET', '/searchClients?query=' + query, true);
             xhr.onload = function() {
                 if (xhr.status === 200) {
                     var data = JSON.parse(xhr.responseText);
@@ -120,6 +154,7 @@ document.addEventListener('DOMContentLoaded', function() {
                         link.addEventListener('click', function(e) {
                             e.preventDefault();
                             searchInput.value = client.name;
+                            userIdDisplay.textContent = '客户身份证：' + client.citizen_id;
                             resultsDropdown.innerHTML = '';
                             resultsDropdown.style.display = 'none';
                         });
@@ -132,3 +167,74 @@ document.addEventListener('DOMContentLoaded', function() {
             xhr.send();
         });
     });
+
+
+
+    //文件上传功能
+    document.getElementById('uploadFileForm').addEventListener('submit', function(e) {
+        e.preventDefault();
+    
+        var formData = new FormData();
+        var fileInput = document.getElementById('fileInput');
+        var fileDescription = document.getElementById('fileDescription').value;
+    
+        formData.append('file', fileInput.files[0]);
+        formData.append('description', fileDescription);
+    
+        fetch('/upload_file', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            // 可以在此处添加代码来处理上传后的响应，如刷新文件列表
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    });
+    
+    //文件列表加载
+    document.addEventListener('DOMContentLoaded', function() {
+        fetch('/get_file_list')
+        .then(response => response.json())
+        .then(files => {
+            var tableBody = document.querySelector('#file-management table tbody');
+            tableBody.innerHTML = ''; // 清空现有内容
+            files.forEach((file, index) => {
+                var row = `<tr>
+                            <th scope="row">${index + 1}</th>
+                            <td>${file.name}</td>
+                            <td>${file.description}</td>
+                            <td>${file.uploadDate}</td>
+                            <td>
+                                <button class="btn btn-sm btn-primary">预览</button>
+                                <a href="${file.downloadUrl}" class="btn btn-sm btn-success" download>下载</a>
+                                <button class="btn btn-sm btn-danger" onclick="deleteFile('${file.id}')">删除</button>
+                            </td>
+                           </tr>`;
+                tableBody.innerHTML += row;
+            });
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    });
+    
+
+    function deleteFile(fileId) {
+        fetch(`/delete_file/${fileId}`, { method: 'DELETE' })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            // 删除成功后重新加载文件列表
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    }
+    
